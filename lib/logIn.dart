98734f58590e7802afd3e0c5f64f2cec522709e3
onedanfc <onedanfc@gmail.com>
2023-03-30 16:12:38 +0700

    frontend login

--- lib/logIn.dart
+++ lib/logIn.dart
@@ -9,19 +9,121 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    final tinggi = MediaQuery.of(context).size.height;
+    final lebar = MediaQuery.of(context).size.width;
     return Scaffold(
         body: Column(
-      children: const [
-        Text(
-          'Ini Halaman masuk',
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              // fontFamily: 'Satoshi',
-              fontSize: 50.0),
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius:
+                const BorderRadius.only(bottomLeft: Radius.circular(100)),
+            color: Colors.amber[600],
+          ),
+          width: lebar,
+          height: tinggi * 0.4,
+          child: Column(
+            children: [
+              SizedBox(
+                height: tinggi * 0.05,
+              ),
+              Image(
+                image: const AssetImage('assets/icon/favicon.png'),
+                width: lebar * 0.3,
+                height: tinggi * 0.2,
+              ),
+            ],
+          ),
         ),
-        Image(image: AssetImage('assets/icon/favicon.png'))
+        SizedBox(
+          height: tinggi * 0.05,
+        ),
+        Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  width: lebar * 0.8,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        alignLabelWithHint: false,
+                        border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 0.5, color: Colors.grey),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.email)),
+                    // validator: (value) {
+                    //   if (value.isEmpty) {
+                    //     return 'Please enter your email';
+                    //   }
+                    //   return null;
+                    // },
+                    // onSaved: (value) {
+                    //   _email = value;
+                    // },
+                  ),
+                ),
+                SizedBox(
+                  height: tinggi * 0.02,
+                ),
+                SizedBox(
+                  width: lebar * 0.8,
+                  child: TextFormField(
+                    decoration:  InputDecoration(
+                      border: const OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 0.5, color: Colors.grey),
+                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: IconButton(onPressed: () {}, icon: const Icon(Icons.remove_red_eye_sharp),),
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                    // validator: (value) {
+                    //   if (value.isEmpty) {
+                    //     return 'Please enter your password';
+                    //   }
+                    //   return null;
+                    // },
+                    // onSaved: (value) {
+                    //   _password = value;
+                    // },
+                  ),
+                ),
+                SizedBox(
+                  height: tinggi * 0.05,
+                ),
+                RawMaterialButton(
+                  onPressed: () {},
+                  fillColor: Colors.amber,
+                  elevation: 2.0,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 8.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  constraints: BoxConstraints(
+                    minWidth: lebar * 0.8,
+                    maxWidth: lebar * 0.8,
+                  ),
+                ),
+              ],
+            ))
       ],
     ));
   }
